b7cf97e69ee659008e81a5b892303c57377b1cc9
Cagatay Beser <besercagatay@gmail.com>
2026-03-08 02:03:54 +0300

    Add RecoveryCodeServices for secure recovery code handling
    Implemented RecoveryCodeServices to generate, hash, and verify cryptographically secure recovery codes using BCrypt. This service supports creating random recovery codes, hashing them, and verifying user input against stored hashes, and implements the IRecoveryCodeService interface.

---
+++ Secure-TOTP-Auth-Sample/TwoFactorAuth.Infrastructure/Services/RecoveryCodeServices.cs
@@ -0,0 +1,32 @@
+﻿using Secure_TOTP_Auth_Sample.TwoFactorAuth.Application.Interfaces;
+using System.Security.Cryptography;
+using BC = BCrypt.Net.BCrypt;
+
+namespace Secure_TOTP_Auth_Sample.TwoFactorAuth.Infrastructure.Services
+{
+    public class RecoveryCodeServices : IRecoveryCodeService
+    {
+        public List<string> GenerateRawCodes(int count = 10)
+        {
+            var codes = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                // Generate a cryptographically random code
+                var bytes = RandomNumberGenerator.GetBytes(5);
+                codes.Add(Convert.ToHexString(bytes).ToLower());
+            }
+            return codes;
+        }
+
+        public string HashCode(string rawCode){
+
+            // Hash the code using BCrypt with a strong work factor
+            return BC.HashPassword(rawCode);
+        }
+
+        public bool VerifyCode(string rawCode, string hashedCode)
+        {
+            return BC.Verify(rawCode, hashedCode);
+        }
+    }
+}
