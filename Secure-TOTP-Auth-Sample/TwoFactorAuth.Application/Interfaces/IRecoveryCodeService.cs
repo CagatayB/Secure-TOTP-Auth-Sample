3a0d2033f4174ffa2b7d54507dd93261695671ab
Cagatay Beser <besercagatay@gmail.com>
2026-03-08 01:48:53 +0300

    Add IRecoveryCodeService interface for 2FA recovery codes
    Introduced IRecoveryCodeService interface to define methods for generating, hashing, and verifying 2FA recovery codes using BCrypt. No implementation included. Also, a BOM was added to the project file with no functional impact.

---
+++ Secure-TOTP-Auth-Sample/TwoFactorAuth.Application/Interfaces/IRecoveryCodeService.cs
@@ -0,0 +1,14 @@
+﻿namespace Secure_TOTP_Auth_Sample.TwoFactorAuth.Application.Interfaces
+{
+    public interface IRecoveryCodeService
+    {
+        // Method to generate a list of recovery codes for 2FA
+        List<string> GenerateRawCodes(int count = 10);
+
+        // Method to generate a hash for thee raw recovery code using BCrypt
+        string HashCode(string rawCode);
+
+        // Method to verify a raw recovery code against its hashed version
+        bool VerifyCode(string rawCode, string hashedCode);
+    }
+}
